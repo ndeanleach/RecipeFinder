344f844b7e68bf6290429d2f9cb26fdf2de4d151
deannate00 <deannate00@gmail.com>
2023-09-28 13:26:50 -0400

    Initial Commit

---
+++ RecipeFinder/RecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderApp.swift
+//  RecipeFinder
+//
+//  Created by Nathan Leach on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
